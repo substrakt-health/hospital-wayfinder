d3e2d96138374aaffdd4fa5c0cccf214816db092
Sam Knight <sam@samknight.co.uk>
2017-01-28 10:59:24 +0000

    Initial Commit

---
+++ Hospital Wayfinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hospital Wayfinder
+//
+//  Created by Sam Knight on 28/01/2017.
+//  Copyright © 2017 Sam Knight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
