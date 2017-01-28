628e0e43be32ae86826cddfa516b3ecd7a1dd6e7
Max Woolf <maximus@zingysaturn.co.uk>
2017-01-28 12:38:37 +0000

    Qr scanner (#2)
    * qr scanner
    
    * .
    
    * add girignore
    
    * fix merge conflicts

---
+++ Hospital Wayfinder/ViewControllers/CameraViewController.swift
@@ -0,0 +1,88 @@
+//
+//  CameraViewController.swift
+//  Hospital Wayfinder
+//
+//  Created by Max Woolf on 28/01/2017.
+//  Copyright © 2017 Sam Knight. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            let input: AnyObject! = try AVCaptureDeviceInput.init(device: captureDevice)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input as! AVCaptureInput)
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            captureSession?.startRunning()
+        } catch {
+            print(error);
+        }
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.green.cgColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubview(toFront: qrCodeFrameView!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        if metadataObjects.count > 0 {
+            captureSession?.stopRunning()
+        
+            let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+            
+            if metadataObj.type == AVMetadataObjectTypeQRCode {
+                let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj as AVMetadataMachineReadableCodeObject) as! AVMetadataMachineReadableCodeObject
+                qrCodeFrameView?.frame = barCodeObject.bounds;
+                
+                if metadataObj.stringValue != nil {
+                    self.performSegue(withIdentifier: "codeRead", sender: metadataObj.stringValue)
+                }
+            }
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "codeRead" {
+            if let destinationVC = segue.destination as? ResultViewController {
+               destinationVC.outputValue = sender as? String
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
